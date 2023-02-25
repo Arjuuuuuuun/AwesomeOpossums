f38d1c7ff4298eee9d70fe91e83c74bd8e43674e
AjNuk <105758636+AjNuk@users.noreply.github.com>
2023-02-24 22:14:09 -0600

    text manager

--- Chillennium2023Game/Assets/Scripts/textManager.cs
+++ Chillennium2023Game/Assets/Scripts/textManager.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class textManager : MonoBehaviour
 {
-    
+
+    public Text tutorial_text;
+    private int counter = 0;
+
+    private void Start()
+    {
+        counter = 0;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (counter)
+        {
+            case (0):
+                tutorial_text.text = "Welcome";
+                break;
+        }
     }
 }
