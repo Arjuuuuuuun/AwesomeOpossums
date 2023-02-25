c4933cdd2e0f415d801bcbec51fb3b8797d25338
jnuccitelli <105758693+jnuccitelli@users.noreply.github.com>
2023-02-24 22:04:53 -0600

    fox

---
+++ Chillennium2023Game/Assets/Scripts/Fox.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    [SerializeField] public int health;
+    [SerializeField] private float speed;
+
+    private void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
