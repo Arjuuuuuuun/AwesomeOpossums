faf91e3b5ee6b795e614973e5952d984c4b3bca0
AjNuk <105758636+AjNuk@users.noreply.github.com>
2023-02-25 17:09:36 -0600

    scene changes

--- Chillennium2023Game/Assets/Scripts/Enemy/EnemySpawn.cs
+++ Chillennium2023Game/Assets/Scripts/Enemy/EnemySpawn.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemySpawn : MonoBehaviour
 {
@@ -17,7 +18,7 @@ public class EnemySpawn : MonoBehaviour
         switch (HeadManager.instance.level_counter)
         {
             case (2):
-
+                Instantiate(infantry, new Vector3(trans.position.x + .5f, trans.position.y, trans.position.z), Quaternion.identity, trans);
                 break;
         }
 
@@ -53,5 +54,7 @@ public class EnemySpawn : MonoBehaviour
         yield return new WaitForSeconds(10);
 
         HeadManager.instance.level_counter = 2;
+        SceneManager.LoadScene(1);
+
     }
 }
