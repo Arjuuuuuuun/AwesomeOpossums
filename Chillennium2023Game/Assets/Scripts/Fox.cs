bc7d089c88fef39bdcd03231374fe6349dbebe6e
Arjun Kurkal <arjunkurkal@tamu.edu>
2023-02-24 22:30:33 -0600

    foxconn

--- Chillennium2023Game/Assets/Scripts/Fox.cs
+++ Chillennium2023Game/Assets/Scripts/Fox.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class Fox : MonoBehaviour
 {
-    [SerializeField] public int health;
-    [SerializeField] private float speed;
-
+    [SerializeField] int health;
+    [SerializeField] float speed;
+    Rigidbody2D rigidbody;
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void Start()
+    {
+        rigidbody.velocity = new Vector3(speed, 0, 0);
+    }
     private void TakeDamage(int damage)
     {
         health -= damage;
+
+    }
+
+    IEnumerator DamageAnime()
+    {
+        speed = -speed;
+
+        yield return new WaitForSeconds(1);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         collision.gameObject.SendMessage("TakeDamage", 6);
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.SendMessage("TakeDamage", 3);
+    }
+
 }
