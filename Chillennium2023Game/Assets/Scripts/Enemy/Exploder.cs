eb33cc43506b58c88e683336b1c6fe21d6cc0200
jnuccitelli <105758693+jnuccitelli@users.noreply.github.com>
2023-02-25 09:23:24 -0600

    explode

--- Chillennium2023Game/Assets/Scripts/Enemy/Exploder.cs
+++ Chillennium2023Game/Assets/Scripts/Enemy/Exploder.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class Exploder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int health;
+    [SerializeField] float speed;
+    [SerializeField] int damage;
+    [SerializeField] float knockBack;
+    [SerializeField] float KBT;
+    [SerializeField] float WT;
+    bool exploding;
+    Rigidbody2D rb;
+    Transform trans;
+
+    private void Awake()
     {
-        
+        exploding = false;
+        rb = GetComponent<Rigidbody2D>();
+        trans = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        rb.velocity = new Vector3(speed, 0, 0);
     }
+    private void TakeDamage(int damage)
+    {
+        StartCoroutine(DamageAnime());
+        health -= damage;
+        if(health <= 0)
+        {
+            StopAllCoroutines();
+            Destroy(this.gameObject);
+        }
+    }
+
+    IEnumerator DamageAnime()
+    {
+        if (this.gameObject != null)
+        {
+            rb.velocity = new Vector3(knockBack * -speed, 0, 0);
+        }
+        yield return new WaitForSeconds(KBT);
+        if (this.gameObject != null)
+        {
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+        yield return new WaitForSeconds(WT);
+        if (this.gameObject != null)
+        {
+            rb.velocity = new Vector3(speed, 0, 0);
+        }
+    }
+
+    IEnumerator Explode()
+    {
+        rb.velocity = Vector3.zero;
+        yield return new WaitForSeconds(2);
+        exploding = true;
+        trans.position = new Vector3(trans.position.x,trans.position.y+100,trans.position.z);
+        Destroy(this.gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(Explode());
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (exploding) { collision.gameObject.SendMessage("TakeDamage", damage); }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(Explode());
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (exploding) { collision.gameObject.SendMessage("TakeDamage", damage); }
+    }
+
 }
