24e9be1fcb39509c846b7e5b1eebeda6a87d4f11
jnuccitelli <105758693+jnuccitelli@users.noreply.github.com>
2023-02-25 11:03:07 -0600

    sceneee

--- Chillennium2023Game/Assets/Scripts/PlayerSpawn.cs
+++ Chillennium2023Game/Assets/Scripts/PlayerSpawn.cs
@@ -7,7 +7,6 @@ public class PlayerSpawn : MonoBehaviour
     public GameObject fox;
     public GameObject rat;
     public GameObject camel;
-    private GameObject clone;
     private Transform trans;
 
     public void Start()
@@ -17,16 +16,16 @@ public class PlayerSpawn : MonoBehaviour
 
     public void SpawnFox()
     {
-        clone = Instantiate(fox, new Vector3(trans.position.x - .5f, trans.position.y, trans.position.z), Quaternion.identity, trans);
+        Instantiate(fox, new Vector3(trans.position.x - .5f, trans.position.y, trans.position.z), Quaternion.identity, trans);
     }
 
     public void SpawnRat()
     {
-        clone = Instantiate(rat, new Vector3(trans.position.x - .5f, trans.position.y, trans.position.z), Quaternion.identity, trans);
+        Instantiate(rat, new Vector3(trans.position.x - .5f, trans.position.y, trans.position.z), Quaternion.identity, trans);
     }
 
     public void SpawnCamel()
     {
-        clone = Instantiate(camel, new Vector3(trans.position.x - .5f, trans.position.y, trans.position.z), Quaternion.identity, trans);
+        Instantiate(camel, new Vector3(trans.position.x - .5f, trans.position.y, trans.position.z), Quaternion.identity, trans);
     }
 }
