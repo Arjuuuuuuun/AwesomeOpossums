b8eebb44f49d63de74fcdceff922c95b4b8b54a8
jnuccitelli <105758693+jnuccitelli@users.noreply.github.com>
2023-02-25 20:03:53 -0600

    enemy spawm

--- Chillennium2023Game/Assets/Scripts/Enemy/EnemySpawn.cs
+++ Chillennium2023Game/Assets/Scripts/Enemy/EnemySpawn.cs
@@ -16,44 +16,44 @@ public class EnemySpawn : MonoBehaviour
     public void Start()
     {
         trans = GetComponent<Transform>();
-        Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y + 2f, trans.position.z), Quaternion.identity, trans);
+        Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y + 0.75f, trans.position.z), Quaternion.identity, trans);
 
-        //switch (HeadManager.instance.level_counter)
-        //{
-        //    case (2):
-        //        Instantiate(infantry, new Vector3(trans.position.x + .5f, trans.position.y, trans.position.z), Quaternion.identity, trans);
-        //        break;
-        //}
+        switch (HeadManager.instance.level_counter)
+        {
+            case (2):
+                Instantiate(infantry, new Vector3(trans.position.x + .5f, trans.position.y -1f, trans.position.z), Quaternion.identity, trans);
+                break;
+        }
 
     }
 
     void Tutur1()
     {
-    //    Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y, trans.position.z), Quaternion.identity, trans);
+        Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y -1f, trans.position.z), Quaternion.identity, trans);
     }
     void StartLevel1()
     {
-    //    StartCoroutine(Level1());
+        StartCoroutine(Level1());
     }
 
-    //IEnumerator Level1()
-    //{
+    IEnumerator Level1()
+    {
 
-    //    //BEGIN LEVEL 1
-    //    Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y + 0, trans.position.z), Quaternion.identity, trans);
-    //    Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y + 1, trans.position.z), Quaternion.identity, trans);
-    //    yield return new WaitForSeconds(6);
+        //BEGIN LEVEL 1
+        Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y + 0.75f, trans.position.z), Quaternion.identity, trans);
+        Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y -1f , trans.position.z), Quaternion.identity, trans);
+        yield return new WaitForSeconds(6);
 
-    //    Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y + 2, trans.position.z), Quaternion.identity, trans);
-    //    Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y + 1, trans.position.z), Quaternion.identity, trans);
-    //    yield return new WaitForSeconds(6);
+        Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y + 2.5f, trans.position.z), Quaternion.identity, trans);
+        Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y -1f, trans.position.z), Quaternion.identity, trans);
+        yield return new WaitForSeconds(6);
 
-    //    Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y + 3, trans.position.z), Quaternion.identity, trans);
-    //    Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y + 0, trans.position.z), Quaternion.identity, trans);
-    //    yield return new WaitForSeconds(10);
+        Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y + 2.5f, trans.position.z), Quaternion.identity, trans);
+        Instantiate(spirit, new Vector3(trans.position.x + .5f, trans.position.y -2.75f, trans.position.z), Quaternion.identity, trans);
+        yield return new WaitForSeconds(10);
 
-    //    HeadManager.instance.level_counter = 2;
-    //    SceneManager.LoadScene(1);
+        HeadManager.instance.level_counter = 2;
+        SceneManager.LoadScene(1);
 
-    //}
+    }
 }
