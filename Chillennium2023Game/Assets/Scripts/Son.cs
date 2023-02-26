0e5bc5c227bb1f7e36610ef00c4a8831644a0f9b
Arjun Kurkal <arjunkurkal@tamu.edu>
2023-02-25 18:52:46 -0600

    father son and holy spirit

--- Chillennium2023Game/Assets/Scripts/Son.cs
+++ Chillennium2023Game/Assets/Scripts/Son.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Son : MonoBehaviour
 {
-
+    Animator anime;
+    SpriteRenderer sprender;
+    [SerializeField] Sprite spir;
+    [SerializeField] Sprite spirt;
+    private void Awake()
+    {
+        sprender = GetComponent<SpriteRenderer>();
+        anime = GetComponent<Animator>();
+    }
     // Update is called once per frame
     void takeDamage(int val)
     {
-        GameObject.Find("GameManager").SendMessage("TakeSonDamage", val);        
+        GameObject.Find("GameManager").SendMessage("TakeSonDamage", val);  
+        anime.StopPlayback();
+        sprender.sprite = spir;
+        StartCoroutine("Damnage");  
+    }
+    IEnumerable Damnage()
+    {
+        yield return new WaitForSeconds(.5f);
+        sprender.sprite = spirt;
+        anime.StartPlayback();
     }
 }
