7462d5502f4ebd32ce413b17f0e6c692ce21fe29
AjNuk <105758636+AjNuk@users.noreply.github.com>
2023-02-26 12:17:43 -0600

    scenes

---
+++ Chillennium2023Game/Assets/Scripts/winManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winManager : MonoBehaviour
+{
+    if (HeadManager.instance.level_counter == 8) {};
+
+}
