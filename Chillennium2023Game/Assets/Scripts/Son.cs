75f0b00b77b224799281c9a9ed33532dc391d66d
Arjun Kurkal <arjunkurkal@tamu.edu>
2023-02-25 20:54:45 -0600

    all praise the father and the son, and all hail the holy spirit

--- Chillennium2023Game/Assets/Scripts/Son.cs
+++ Chillennium2023Game/Assets/Scripts/Son.cs
@@ -6,28 +6,26 @@ public class Son : MonoBehaviour
 {
     Animator anime;
     SpriteRenderer sprender;
-    [SerializeField] Sprite spir;
-    [SerializeField] Sprite spirt;
+    
     private void Awake()
     {
-        sprender = GetComponent<SpriteRenderer>();
         anime = GetComponent<Animator>();
+
+        anime.SetBool("funnyDeathBool", false);
+        sprender = GetComponent<SpriteRenderer>();
     }
     // Update is called once per frame
     void takeDamage(int val)
     {
-        GameObject.Find("GameManager").SendMessage("TakeSonDamage", val);  
-        anime.StopPlayback();
-        sprender.sprite = spir;
-        sprender.sortingOrder = 2;
-        StartCoroutine("Damnage");  
+        GameObject.Find("GameManager").SendMessage("TakeSonDamage", val);          
+        StartCoroutine(Damnage());  
     }
-    IEnumerable Damnage()
+    IEnumerator Damnage()
     {
-        yield return new WaitForSeconds(.5f);
-        sprender.sprite = spirt;
-        sprender.sortingOrder = 1;
+        anime.SetBool("funnyDeathBool", true);
 
-        anime.StartPlayback();
+        yield return new WaitForSeconds(.5f);
+        anime.SetBool("funnyDeathBool", false);
+
     }
 }
