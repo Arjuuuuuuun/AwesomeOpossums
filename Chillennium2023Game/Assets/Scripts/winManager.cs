322eba2d5e090bbcda9a8362158a90db38ae8463
AjNuk <105758636+AjNuk@users.noreply.github.com>
2023-02-26 12:22:22 -0600

    win manager

--- Chillennium2023Game/Assets/Scripts/winManager.cs
+++ Chillennium2023Game/Assets/Scripts/winManager.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class winManager : MonoBehaviour
 {
-    if (HeadManager.instance.level_counter == 8) {};
+    public Text text;
+    public Button button;
+    private void Update()
+    {
+        if (HeadManager.instance.level_counter == 8)
+        {
+            text.text = "You Win!";
+            button.transform.position = new Vector3(10000, 10000, 10000);
+        }
+    }
+
 
 }
