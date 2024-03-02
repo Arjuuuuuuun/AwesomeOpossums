6ee3522b0bbf0e1f96ac9db396c144d4fc5ea847
anuccitelli2 <anuccitelli2@tamu.edu>
2024-03-02 10:44:11 -0600

    Made the fade out audio better

--- Chillennium2024Game/Assets/Scripts/AudioManager.cs
+++ Chillennium2024Game/Assets/Scripts/AudioManager.cs
@@ -6,20 +6,24 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
 
-    public static AudioSource[] aud = new AudioSource[2];
-    [SerializeField] GameObject heartbeatg;
-    AudioSource heartbeat;
+    [SerializeField] GameObject LevelThemeg;
+    [SerializeField] GameObject Heartbeatg;
+    AudioSource Heartbeat;
+    AudioSource LevelTheme;
     // Start is called before the first frame update
     void Awake()
-    {
-        heartbeat = heartbeatg.GetComponent<AudioSource>();
+    {   
+        LevelTheme = LevelThemeg.GetComponent<AudioSource>();
+        Heartbeat = Heartbeatg.GetComponent<AudioSource>();
+        Heartbeat.Play();
+        LevelTheme.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log((Player.health - 10f) / 10f);
-        heartbeat.volume = (10f - Player.health)  / 10f;
+        Heartbeat.volume = (10f - Player.health)  / 10f;
         
     }
 }
