1e09c7d57c2ae04dbe37598d9f3e86ecb4f188b6
Arjun Kurkal <arjunkurkal@tamu.edu>
2024-03-02 09:58:09 -0600

    spawner

--- Chillennium2024Game/Assets/Scripts/Spawner.cs
+++ Chillennium2024Game/Assets/Scripts/Spawner.cs
@@ -6,24 +6,26 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject enemy;
     [SerializeField] private int direction;
+    GameObject headManager;
 
     private Transform transform;
     // Start is called before the first frame update
     void Start()
     {
+        headManager = GameObject.FindGameObjectWithTag("HeadManager");
         transform = GetComponent<Transform>();
-        StartLevel(0);
+        StartLevel(HeadManager.level_counter);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void StartLevel(int level)
     {
-        switch(level)
+        switch (level)
         {
             case 0:
                 StartCoroutine("LevelOne");
@@ -46,14 +48,36 @@ public class Spawner : MonoBehaviour
 
     IEnumerator LevelOne()
     {
-        
 
-        while (true)
+        ArrayList list = new ArrayList();
+
+
+        for (int i = 0; i < 5; ++i)
         {
             GameObject enemy = Instantiate(this.enemy);
             enemy.SendMessage("Rotate", direction);
             enemy.transform.position = transform.position;
+            list.Add(enemy);
             yield return new WaitForSeconds(3);
         }
+
+        while (true)
+        {
+            int num_alive = 0;
+            for (int i = 0; i < list.Count; ++i)
+            {
+                if (list[i] != null) { 
+                    num_alive++; 
+                    break; 
+                }
+            }
+            if (num_alive == 0) 
+                break; 
+            yield return new WaitForSeconds(2);
+
+        }
+
+
+        headManager.SendMessage("LevelComplete");
     }
 }
