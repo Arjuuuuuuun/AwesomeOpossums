0098a715d562b1b625f908748a02159cd62ea086
Arjun Kurkal <arjunkurkal@tamu.edu>
2024-03-01 23:16:18 -0600

    rotation

--- Chillennium2024Game/Assets/Scripts/Obstacle.cs
+++ Chillennium2024Game/Assets/Scripts/Obstacle.cs
@@ -7,9 +7,8 @@ public class Obstacle : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private int direction; // 1,2,3,4 for left right up down respectively
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-        collision.gameObject.SendMessage("rotate", direction);
+        collision.gameObject.SendMessage("Rotate", direction);
     }
 }
