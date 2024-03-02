c0db187ccd5dea8ced9fa6fe62bda969349f57b8
jnuccitelli <jnuccitelli1@gmail.com>
2024-03-02 09:10:40 -0600

    head manager

---
+++ Chillennium2024Game/Assets/Scripts/HeadManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadManager : MonoBehaviour
+{
+    public static HeadManager instance;
+    public static int level_counter;
+
+    private void Awake()
+    {
+
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
