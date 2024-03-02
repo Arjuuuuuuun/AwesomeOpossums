8f7a6b5c9625e57749290aa82ed9c0bc5ccb219f
anuccitelli2 <anuccitelli2@tamu.edu>
2024-03-02 16:34:06 -0600

    changed some position values

--- Chillennium2024Game/Assets/Scripts/GhostSpawner.cs
+++ Chillennium2024Game/Assets/Scripts/GhostSpawner.cs
@@ -30,8 +30,8 @@ public class GhostSpawner : MonoBehaviour
     {
         GameObject ghost = Instantiate(this.ghost);
         ghosts.Add(ghost);
-        ghost.SendMessage("init", new Vector2(10, 10));
-        ghost.SendMessage("init2", new Vector2(1, 0));
+        ghost.SendMessage("init", new Vector2(5, 5));
+        ghost.SendMessage("init2", new Vector2(0, -1));
         yield return new WaitForSeconds(1f);
     }
 }
