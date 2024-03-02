aab10b0c105100a1a04e506a673f91dab4318e53
anuccitelli2 <anuccitelli2@tamu.edu>
2024-03-02 17:30:38 -0600

    Even more ghost

--- Chillennium2024Game/Assets/Scripts/GhostSpawner.cs
+++ Chillennium2024Game/Assets/Scripts/GhostSpawner.cs
@@ -63,6 +63,16 @@ public class GhostSpawner : MonoBehaviour
         {
             SummonGhost(new Vector2(10 *Mathf.Cos(i*30), 10 * Mathf.Sin(i * 30)), new Vector2(-10 * Mathf.Cos(i * 30), -10 * Mathf.Sin(i * 30)));
         }
+        yield return new WaitForSeconds(0.5f);
+        SummonGhost(new Vector2(15, 5.5f), new Vector2(-1, -1));
+        SummonGhost(new Vector2(12, 5.5f), new Vector2(-1, -1));
+        SummonGhost(new Vector2(9, 5.5f), new Vector2(-1, -1));
+        SummonGhost(new Vector2(6, 5.5f), new Vector2(-1, -1));
+        SummonGhost(new Vector2(3, 5.5f), new Vector2(-1, -1));
+        SummonGhost(new Vector2(0, 5.5f), new Vector2(-1, -1));
+        SummonGhost(new Vector2(-3, 5.5f), new Vector2(-1, -1));
+        SummonGhost(new Vector2(-6, 5.5f), new Vector2(-1, -1));
+        SummonGhost(new Vector2(-9, 5.5f), new Vector2(-1, -1));
     }
 
     private void SummonGhost(Vector2 position, Vector2 velocity)
