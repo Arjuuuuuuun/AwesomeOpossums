c3970942da662455a12bebd0e3de25878f24afb7
Arjun Kurkal <arjunkurkal@tamu.edu>
2024-03-01 22:56:18 -0600

    updates

--- Chillennium2024Game/Assets/Scripts/Obstacle.cs
+++ Chillennium2024Game/Assets/Scripts/Obstacle.cs
@@ -6,16 +6,6 @@ public class Obstacle : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private int direction; // 1,2,3,4 for left right up down respectively
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
