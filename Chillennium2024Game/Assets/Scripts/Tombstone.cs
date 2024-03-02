cf8b62a4a84b8250da48213a55b0be6a07fea3c5
jnuccitelli <jnuccitelli1@gmail.com>
2024-03-01 22:46:41 -0600

    tombstone inhretince

--- Chillennium2024Game/Assets/Scripts/Tombstone.cs
+++ Chillennium2024Game/Assets/Scripts/Tombstone.cs
@@ -1,29 +1,23 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Tombstone : MonoBehaviour
 {
 
     [SerializeField] protected enum TombstoneState {notActive, Active };
-    TombstoneState state;
-    [SerializeField] float tombstoneAwakeTime;
-    
-    bool canFire;
-    [SerializeField] GameObject bullet;
-    [SerializeField] float fireRate;
-    [SerializeField] float timeBeforeFirstFire;
+    protected TombstoneState state;
+    [SerializeField] protected float tombstoneAwakeTime;
     bool playerNear;
 
-    private SpriteRenderer spriteRenderer;
+
+
+    protected SpriteRenderer spriteRenderer;
    
     void Start()
     {
         state = TombstoneState.notActive;
         spriteRenderer = GetComponent<SpriteRenderer>();
-        canFire = true;
+
     }
 
     void Update()
@@ -38,15 +32,15 @@ public class Tombstone : MonoBehaviour
                 }
                 break;
             case TombstoneState.Active:
+                Activate();
                 spriteRenderer.color = Color.blue;
-                if (canFire){
-                    spriteRenderer.color = Color.red;
-                    StartCoroutine(Fire());
-                }
                 break;   
         }
     }
-
+    virtual protected void Activate()
+    {
+        return;
+    }
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -62,26 +56,13 @@ public class Tombstone : MonoBehaviour
             playerNear = false;
         }
     }
+
     IEnumerator ActiveTimer()
     {
         state = TombstoneState.Active;
-        canFire = false;
-        yield return new WaitForSeconds(timeBeforeFirstFire);
-        canFire = true;
         yield return new WaitForSeconds(tombstoneAwakeTime);
         state = TombstoneState.notActive;
     }
-    IEnumerator Fire()
-    {
-        canFire = false;
-        for (int i = 0; i < 360; i += 90)
-        {
-            GameObject b = Instantiate(bullet, this.transform.position, Quaternion.identity);
-            b.gameObject.SendMessage("MakeGo",i);
-            yield return new WaitForSeconds(0.5f);
-        }
-        yield return new WaitForSeconds(fireRate);
-        canFire = true;
-    }
+
 
 }
