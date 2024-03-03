fa85c7d70fe76b450b2bdceff8673f3796e7a2bc
Arjun Kurkal <arjunkurkal@tamu.edu>
2024-03-03 08:45:17 -0600

    cleaned up code

--- Chillennium2024Game/Assets/Scripts/LevelButtonController.cs
+++ Chillennium2024Game/Assets/Scripts/LevelButtonController.cs
@@ -16,14 +16,6 @@ public class LevelButtonController : MonoBehaviour
     void Update()
     {
         b.interactable = !Spawner.in_level;
-        if (b.interactable)
-        {
-            
-        }
-        else
-        {
-     
-        }
 
     }
 }
