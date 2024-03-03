62ccf1a75a66a40aac48177a61ee740e962691b1
Arjun Kurkal <arjunkurkal@tamu.edu>
2024-03-02 23:00:07 -0600

    UI

---
+++ Chillennium2024Game/Assets/Scripts/Heart.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+
+    [SerializeField] SpriteRenderer heart1;
+    [SerializeField] SpriteRenderer heart3;
+    [SerializeField] SpriteRenderer heart2;
+    [SerializeField] SpriteRenderer logo;
+
+    [SerializeField] Sprite full_heart;
+    [SerializeField] Sprite empty_heart;
+    [SerializeField] Sprite half_heart;
+    [SerializeField] Sprite blue_balls;
+    [SerializeField] Sprite red_balls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player.tombstone == Player.TombstoneType.radial)
+        {
+            logo.sprite = blue_balls;
+            logo.gameObject.transform.localScale = new Vector3(.5f,.5f,.5f);
+        }
+        else
+        {
+            logo.sprite = red_balls;
+            logo.gameObject.transform.localScale = new Vector3(1,1,1);
+
+        }
+        if (Player.life == Player.Life.Dead)
+        {
+            switch(Player.dead_health)
+            {
+                case 0:
+                    heart1.color = new Color(0f, 0f, 0f, 0f);
+                    heart2.sprite = empty_heart;
+                    heart3.sprite = half_heart;
+                    break;
+                case 1:
+
+                    heart1.color = new Color(0f, 0f, 0f, 0f);
+                    heart2.sprite = half_heart;
+                    heart3.sprite = half_heart;
+                    break;
+            }
+        }
+        else
+        {
+            heart1.color = new Color(1f, 1f, 1f, 1f);
+
+            switch (Player.health)
+            {
+                case 0:
+                    heart1.sprite = empty_heart;
+                    heart2.sprite = empty_heart;
+                    heart3.sprite = full_heart;
+                    break;
+                case 1:
+                    heart1.sprite = empty_heart;
+                    heart2.sprite = full_heart;
+                    heart3.sprite = full_heart;
+                    break;
+                case 2:
+                    heart1.sprite = full_heart;
+                    heart2.sprite = full_heart;
+                    heart3.sprite = full_heart;
+                    break;
+            }
+        }
+    }
+}
