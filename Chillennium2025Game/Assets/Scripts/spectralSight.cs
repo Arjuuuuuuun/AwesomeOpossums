c7dd9d1349f4a6c15d3dc6f4bc91e4a41c617e56
jnuccitelli <jnuccitelli1@gmail.com>
2025-02-08 09:10:57 -0600

    arjun fixes

--- Chillennium2025Game/Assets/Scripts/spectralSight.cs
+++ Chillennium2025Game/Assets/Scripts/spectralSight.cs
@@ -22,9 +22,18 @@ public class spectralSight : MonoBehaviour
         if (!inSight)
         {
             float distanceToPlayer = Vector2.Distance(FindAnyObjectByType<PlayerMovement>().gameObject.transform.position,this.transform.position);
-            if (distanceToPlayer > appearOuterRadius)
+            if (distanceToPlayer < appearInnerRadius)
             {
-
+                new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            }
+            else if(distanceToPlayer < appearOuterRadius)
+            {
+                float scratch = distanceToPlayer - appearOuterRadius;
+                new Color(1.0f, 1.0f, 1.0f,scratch / (appearOuterRadius - appearInnerRadius));
+            }
+            else
+            {
+                spriteRenderer.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
             }
         }
     }
@@ -32,12 +41,12 @@ public class spectralSight : MonoBehaviour
     public void toggleOnSpectralLayer()
     {
         inSight = true;
-        spriteRenderer.enabled = true;
+        spriteRenderer.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
     }
 
     public void toggleOffSpectralLayer()
     {
         inSight = false;
-        spriteRenderer.enabled = false;
+        spriteRenderer.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
     }
 }
