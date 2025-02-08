cce551ccde32aa394702e88fa2ecba40c45300c8
jnuccitelli <jnuccitelli1@gmail.com>
2025-02-08 13:11:05 -0600

    sight

---
+++ Chillennium2025Game/Assets/Scripts/homingGhostScript.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class homingGhostScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float velocity;
+    PlayerMovement player;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = FindAnyObjectByType<PlayerMovement>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (player.spectralOn)
+        {
+            Vector3 direction = -(transform.position - player.transform.position).normalized;
+            rb.velocity = new Vector2(direction.x * velocity, direction.y * velocity);
+        }
+        else {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
