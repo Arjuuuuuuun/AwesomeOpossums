302468f5163b0fc43b9251ba5fc10d455d0b7205
jnuccitelli <jnuccitelli1@gmail.com>
2025-02-08 21:14:56 -0600

    ghost start pos on path

--- Chillennium2025Game/Assets/Scripts/GhostScript.cs
+++ Chillennium2025Game/Assets/Scripts/GhostScript.cs
@@ -9,6 +9,7 @@ public class GhostScript : MonoBehaviour
     [SerializeField] float xVelocity;
     [SerializeField] float yVelocity;
     [SerializeField] float patrolDistance;
+    [SerializeField] float initDistanceOnPath;
     private Transform trans;
     void Start()
     {
@@ -19,16 +20,24 @@ public class GhostScript : MonoBehaviour
 
     IEnumerator Patrol()
     {
+        bool firstTime = true;
+        patrolDistance -= initDistanceOnPath;
         while (true)
-        {
+        { 
             rb.velocity = new Vector2(xVelocity, yVelocity);
             if (xVelocity > 0)  trans.localScale = new Vector3(1, 1,1);  
             else trans.localScale = new Vector3(-1, 1,1);
             yield return new WaitForSeconds(patrolDistance / MathF.Sqrt(xVelocity * xVelocity + yVelocity * yVelocity));
+            if (firstTime)
+            {
+                patrolDistance += initDistanceOnPath;
+                firstTime = false;
+            }
             rb.velocity = new Vector2(-xVelocity,   -yVelocity);
             if (xVelocity <= 0) trans.localScale = new Vector3(1,1, 1);
             else trans.localScale = new Vector3(-1,1, 1);
             yield return new WaitForSeconds(patrolDistance / MathF.Sqrt(xVelocity * xVelocity + yVelocity * yVelocity));
+
         }
     }
 }
